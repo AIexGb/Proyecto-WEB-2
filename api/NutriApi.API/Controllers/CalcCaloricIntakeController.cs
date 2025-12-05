70482e241cdfde1d7a416172b3dcf36a82e91b45
carlosperez2207 <dg753513@gmail.com>
2025-12-05 14:56:09 -0500

    controladores

--- api/NutriApi.API/Controllers/CalcCaloricIntakeController.cs
+++ api/NutriApi.API/Controllers/CalcCaloricIntakeController.cs
@@ -8,8 +8,34 @@ namespace NutriApi.API.Controllers
 	[Route("/[controller]")]
 	public class CalcCaloricIntakeController : ControllerBase
 	{
-		
+        private readonly ICalcCaloricIntakeService _calcCaloricIntakeService;
+
+        public CalcCaloricIntakeController(ICalcCaloricIntakeService calcCaloricIntakeService)
+        {
+            _calcCaloricIntakeService = calcCaloricIntakeService;
+        }
+
+        [HttpPost("calcular-kcal-intake")]
+        public async Task<IActionResult> CalcCaloricIntakeAsync([FromBody] KcalRequestDto request)
+        {
+            CalculoResponseDTO result = await _calcCaloricIntakeService.CalcCaloricIntakeAsync(request.Gender, request.Weight, request.Height, request.Activity, request.Objective);
+            return Ok(result);
+        }
+
+        [HttpGet("calcular-kcal-intake-GET")]
+        public async Task<IActionResult> CalcCaloricIntakeAsync(
+        bool gender,
+        double weight,
+        int height,
+        double activity,
+        int objective)
+        {
+            CalculoResponseDTO result = await _calcCaloricIntakeService.CalcCaloricIntakeAsync(
+                gender, weight, height, activity, objective);
+
+            return Ok(result);
+        }
 
 
-	}
+    }
 }
