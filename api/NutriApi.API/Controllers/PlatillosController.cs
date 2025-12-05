4c6b18e56a00bea35e784189e56a0120601b0c89
carlosperez2207 <dg753513@gmail.com>
2025-12-05 15:00:35 -0500

    controlador

--- api/NutriApi.API/Controllers/PlatillosController.cs
+++ api/NutriApi.API/Controllers/PlatillosController.cs
@@ -10,6 +10,63 @@ namespace NutriApi.API.Controllers
 	[Route("/[controller]")]
 	public class PlatillosController : ControllerBase
 	{
-		
-	}
+        private readonly IKcalService _kcalService;
+        private readonly ILogService _logService;
+        private readonly IGenericServiceAsync<Platillo, PlatilloDTO> _platilloService;
+        private readonly IPlatilloService _platilloServiceSpecial;
+
+
+        public PlatillosController(IKcalService kcalService, ILogService logService, IGenericServiceAsync<Platillo, PlatilloDTO> platilloService, IPlatilloService platilloServiceSpecial)
+        {
+            _kcalService = kcalService;
+            _logService = logService;
+            _platilloService = platilloService;
+            _platilloServiceSpecial = platilloServiceSpecial;
+        }
+
+        [HttpPost("{platilloId}/calcular-kcal")]
+        public async Task<IActionResult> CalcularKcalPlatilloAsync(int platilloId)
+        {
+            var total = await _kcalService.CalcularKcalPlatilloAsync(platilloId);
+
+            await _logService.AddLogAsync(
+                dispositivo: Request.Headers["User-Agent"].ToString() ?? "Unknown",
+                operacion: $"Calculo kcal platillo {platilloId}",
+                tabla: "Platillo"
+            );
+
+            return Ok(new { PlatilloId = platilloId, TotalKcal = total });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var platillos = await _platilloServiceSpecial.GetAllWithIngredientesAsync();
+            return Ok(platillos);
+        }
+
+        [HttpGet("byname")]
+        public async Task<IActionResult> GetByNameAsync(string name)
+        {
+            var platillos = await _platilloServiceSpecial.GetByNameAsync(name);
+            return Ok(platillos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var platillos = await _platilloService.GetByIdAsync(id);
+            return Ok(platillos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] PlatilloCreateDTO dto)
+        {
+            var result = await _platilloServiceSpecial.CreatePlatilloAsync(dto);
+            await _logService.AddLogAsync(Request.Headers["User-Agent"].ToString(), $"Created new dish {dto.Nombre}", "Platillo");
+
+
+            return Ok(new { Message = "Platillo created successfully" });
+        }
+    }
 }
