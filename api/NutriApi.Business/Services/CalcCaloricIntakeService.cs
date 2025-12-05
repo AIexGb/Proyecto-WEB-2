44b259e0a23c5ce83843ea91ffde5fad86a2c3c5
Aki-142 <@Naoyalemessi500>
2025-12-05 16:28:43 -0700

    Servicios de Calcular

--- api/NutriApi.Business/Services/CalcCaloricIntakeService.cs
+++ api/NutriApi.Business/Services/CalcCaloricIntakeService.cs
@@ -13,5 +13,55 @@ namespace NutriApi.Business.Services
 
 		public async Task<CalculoResponseDTO> CalcCaloricIntakeAsync(bool gender, double weight, int height, double activity, int objective)
 		{
+            //use this comment to define the objectives (1 = downwheight, 2 = mantain, 3 = upweight)
+            //use this comment to define the gender variable (true = hombre, false = mujer)
+
+            double heightIN = height / 2.54;
+            double PI;      //stands for "Peso Ideal"
+            double PA;      //stands for "Peso Ajustado"
+            double GER;     //stands for "Gasto Energetico en Reposo"
+            double TDEE;    //stands for "Total Daily Energy Expenditure" o "Gasto total energetico diario"
+            double ajuste = 0;
+            double waterConsumption = 0;
+
+            if (gender)
+            {
+                PI = 50 + 2.3 * (heightIN - 60);
+            }
+            else
+            {
+                PI = 45.5 + 2.3 * (heightIN - 60);
+            }
+
+            PA = (weight - PI) * 0.25 + PI;
+
+            GER = 25 * PA;
+
+            TDEE = GER * activity;
+
+            if (objective == 1)//downwheight
+            {
+                ajuste = -300;
+            }
+            if (objective == 2)//mantain
+            {
+                ajuste = 0;
+            }
+            if (objective == 3)//upweight
+            {
+                ajuste = 300;
+            }
+
+            waterConsumption = weight * 0.035;
+
+            return new CalculoResponseDTO
+            {
+                GER = Math.Round(GER, 2),
+                TDEE = Math.Round(TDEE, 2),
+                TDEE_Ajustado = Math.Round(TDEE + ajuste, 2),
+                DailyWater = Math.Round(waterConsumption, 2),
+                Mensaje = "OK"
+            };
+        }
 	}
 }
--- api/NutriApi.Business/Services/GenericServiceAsync.cs
+++ api/NutriApi.Business/Services/GenericServiceAsync.cs
@@ -12,5 +12,28 @@ namespace NutriApi.Business.Services
 	where TEntity : class
 	where TDto : class
 	{
-	}
+        private readonly IGenericRepository<TEntity> _genericRepository;
+        private readonly IMapper _mapper;
+
+        public GenericServiceAsync(IGenericRepository<TEntity> genericRepository, IMapper mapper) : base(genericRepository, mapper)
+        {
+            _genericRepository = genericRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(TDto dto)
+        {
+            await _genericRepository.AddAsync(_mapper.Map<TEntity>(dto));
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            await _genericRepository.DeleteByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(TDto dto)
+        {
+            await _genericRepository.UpdateAsync(_mapper.Map<TEntity>(dto));
+        }
+    }
 }
