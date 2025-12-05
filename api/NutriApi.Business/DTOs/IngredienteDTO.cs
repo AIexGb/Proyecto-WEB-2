b8647001fd5680e5804e7269c6a8934ec8b29610
carlosperez2207 <dg753513@gmail.com>
2025-12-05 15:04:50 -0500

    dto

--- api/NutriApi.Business/DTOs/IngredienteDTO.cs
+++ api/NutriApi.Business/DTOs/IngredienteDTO.cs
@@ -9,7 +9,13 @@ namespace NutriApi.Business.DTOs
 {
 	public class IngredienteDTO
 	{
-	
 
+        public int IngredienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public double Kcal { get; set; }
+        public int proteinaG { get; set; }
+        public int grasasG { get; set; }
+        public int carbosG { get; set; }
     }
 }
