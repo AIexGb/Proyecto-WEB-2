5ec62fb38e43b26919ace26d0c8e4b5c25318791
carlosperez2207 <dg753513@gmail.com>
2025-12-05 14:59:30 -0500

    controlador

--- api/NutriApi.API/Controllers/IngredienteController.cs
+++ api/NutriApi.API/Controllers/IngredienteController.cs
@@ -10,8 +10,21 @@ namespace NutriApi.API.Controllers
 	[Route("/[controller]")]
 	public class IngredienteController : ControllerBase
 	{
-		
+        private readonly ILogService _logService;
+        private readonly IGenericServiceAsync<Ingrediente, IngredienteDTO> _ingredienteService;
 
+        public IngredienteController(ILogService logService, IGenericServiceAsync<Ingrediente, IngredienteDTO> ingredienteService)
+        {
+            _logService = logService;
+            _ingredienteService = ingredienteService;
+        }
 
-	}
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var ingredientes = await _ingredienteService.GetAllAsync();
+            return Ok(ingredientes);
+        }
+
+    }
 }
